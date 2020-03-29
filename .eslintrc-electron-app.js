1300c259cfec37463e8a68eeda5a8f0ac48ac952
Ryan Cole <ryan@askdesigners.eu>
2020-03-29 15:25:37 +0200

    first basic backend

--- .eslintrc-electron-app.js
+++ .eslintrc-electron-app.js
@@ -1,38 +1,25 @@
 "use strict";
 
 module.exports = {
-	"env": {
-		"commonjs": true,
-		"es6": true,
-		"node": true
-	},
-	"extends": "eslint:recommended",
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
-	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"parser": "babel-eslint"
-	},
-	"rules": {
-		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: "eslint:recommended",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        parser: "babel-eslint"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
+    }
 };
--- src/electron-app/main.js
+++ src/electron-app/main.js
@@ -1,79 +1,88 @@
 "use strict";
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
-const { resolve } = require("path");
-const { format } = require("url");
+import { app, BrowserWindow, ipcMain } from "electron";
+import { resolve } from "path";
+import { format } from "url";
+
+import dataFile from "../../mapData/mapStore.json";
+import mapActions from "./mapActions";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow, mapActionsInstance;
 
-function createWindow () {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		icon: resolve(__dirname, "icon.png"),
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        icon: resolve(__dirname, "icon.png"),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-	// Remove menu from browser window.
-	mainWindow.setMenu(null);
+    // Remove menu from browser window.
+    mainWindow.setMenu(null);
 
-	// Load the index.html of the app.
-	mainWindow.loadURL(process.env.NODE_ENV === "development" ? format({
-		hostname: "localhost",
-		pathname: "index.html",
-		protocol: "http",
-		slashes: true,
-		port: 8080
-	}) : format({
-		pathname: resolve(__dirname, "..", "vue-app/index.html"),
-		protocol: "file",
-		slashes: true
-	}));
+    // Load the index.html of the app.
+    mainWindow.loadURL(
+        process.env.NODE_ENV === "development"
+            ? format({
+                  hostname: "localhost",
+                  pathname: "index.html",
+                  protocol: "http",
+                  slashes: true,
+                  port: 8080
+              })
+            : format({
+                  pathname: resolve(__dirname, "..", "vue-app/index.html"),
+                  protocol: "file",
+                  slashes: true
+              })
+    );
 
-	// Open the DevTools.
-	if(process.env.NODE_ENV === "development") {
-		mainWindow.webContents.openDevTools();
-		require("devtron").install();
-		require("vue-devtools").install();
-	}
+    // Open the DevTools.
+    if (process.env.NODE_ENV === "development") {
+        mainWindow.webContents.openDevTools();
+        require("devtron").install();
+        require("vue-devtools").install();
+    }
 
-	// Emitted when the window is closed.
-	mainWindow.on("closed", () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+    // Emitted when the window is closed.
+    mainWindow.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
-	createWindow();
+    createWindow();
+    mapActionsInstance = new mapActions(ipcMain, dataFile);
 });
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if(process.platform !== "darwin") {
-		app.quit();
-	}
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    mapActionsInstance.kill();
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if(mainWindow === null) {
-		createWindow();
-	}
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
